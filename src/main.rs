0c8caa8edd24baf60643fc88274b81ffa2e17f2a
raymond zhao <raymnzd@gmail.com>
2020-04-16 03:52:48 -0400

    added case insensitive

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use minigrep::Config;
-use std::env;
 use std::process;
+use std::env;
+use minigrep::Config;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,10 +9,16 @@ fn main() {
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
-    });
+    }); 
 
-    if let Err(e) = minigrep::run(config) {
-        println!("Application error : {}", e);
+    
+
+    if let Err(e) = minigrep::run(config){
+        println!("Application error : {}",e);
         process::exit(1);
     };
+    
 }
+
+
+

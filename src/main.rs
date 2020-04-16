dc9b2f66d5eb9cca5171cd8878e7aea9af934edc
raymond zhao <raymnzd@gmail.com>
2020-04-16 00:52:31 -0400

    add search test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use std::process;
-use std::env;
 use minigrep::Config;
-
+use std::env;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,16 +8,10 @@ fn main() {
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
-    }); 
+    });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
-    if let Err(e) = minigrep::run(config){
-        println!("Application error : {}",e);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error : {}", e);
         process::exit(1);
     };
 }
-
-
-
